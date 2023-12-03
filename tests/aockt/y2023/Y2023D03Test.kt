dbc256071e18a6e36428e6cec4bd1d0ae0c02f56
Josef H.B. Schneider <git@netpage.dk>
2023-12-03 22:00:22 +0100

    day3 more tests

--- tests/aockt/y2023/Y2023D03Test.kt
+++ tests/aockt/y2023/Y2023D03Test.kt
@@ -47,6 +47,25 @@ private val redditTest2 = """
     1.1..503+.56
 """.trimIndent()
 
+private val redditTest3 = """
+    ....................
+    ..-52..52-..52..52..
+    ..................-.
+""".trimIndent()
+
+private val redditTest4 = """
+    ........
+    .24..4..
+    ......*.
+""".trimIndent()
+
+private val redditTest5 = """
+    .......5......
+    ..7*..*.......
+    ...*13*.......
+    .......15.....
+""".trimIndent()
+
 @AdventDay(2023, 3, "Gear Ratios")
 class Y2023D03Test : AdventSpec<Y2023D03>({
 
@@ -54,12 +73,18 @@ class Y2023D03Test : AdventSpec<Y2023D03>({
         officialTestcase shouldOutput 4361
         redditTest1 shouldOutput 413
         redditTest2 shouldOutput 925
+        redditTest3 shouldOutput 156
+        redditTest4 shouldOutput 4
+        redditTest5 shouldOutput 40
     }
 
     partTwo {
         officialTestcase shouldOutput 467835
         redditTest1 shouldOutput 6756
         redditTest2 shouldOutput 6756
+        redditTest3 shouldOutput 0
+        redditTest4 shouldOutput 0
+        redditTest5 shouldOutput 442
         "333.3\n...*." shouldOutput 999
     }
 
